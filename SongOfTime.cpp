ae157c0ca38c495b5f0fb4eda665ffb71ca5cd1e
Jason Gerlowski <jeg90@pitt.edu>
2012-12-16 14:12:44 -0500

    Mode definition

--- SongOfTime.cpp
+++ SongOfTime.cpp
@@ -1,19 +1,26 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int
 main(int argc, char *argv[ ])
 {
+	//Used for reading retval from getopt
     	int c;
+
+	//Create the different mode flags
     	int errFlag=0;
 	int startFlag=0;
 	int finishFlag=0;
 
+	//Start Title 
     	char *startTitle=NULL;
 
+	//getopt flags and variables
     	extern char *optarg;
     	extern int optind, optopt;
 
+	//Iterate through command line options to set mode flags
     	while ((c = getopt(argc, argv, "s:f?")) != -1) 
 	{
         	switch(c) 
@@ -34,9 +41,35 @@ main(int argc, char *argv[ ])
         	}
     	}
 
+	//Error case: bad args configuration
 	if(errFlag==1 || (startFlag==1 && finishFlag==1) || (startFlag==0 && finishFlag==0))
 	{
 		printf("\nImproper arguments, Try: SongOfTime [-s TaskName] [-f]");
+	}
+	else if(startFlag==1)//Start command
+	{
+		//check if a task is already being tracked- (already open)
+		int alreadyOpen;
+		if(alreadyOpen==1)//Print error message and quit
+		{
+			printf("\nA task is already being tracked.  Please close task before continuing");
+			printf("\n\tTry: songoftime -f");
+			exit(0);
+		}
+		
+		
+	}
+	else if(finishFlag==1)//Task finish command
+	{
+		//Check if a task is currently being tracked (already open)
+		int alreadyOpen;
+		if(!alreadyOpen==0)//If no task is open, print error message and quit
+		{
+			printf("A task is not currently being tracked.  Please set task to be tracked");
+			printf("\n\tTry: songoftime -s \"TaskName\"");
+			exit(0);
+		}
+		
 	}
 
     
